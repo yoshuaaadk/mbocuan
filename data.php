c537b76fbfaac61484c877a7aeffbf0ad83519a4
yoshuaaadk <65962962+yoshuaaadk@users.noreply.github.com>
2020-05-30 13:11:23 +0700

    Add files via upload
    Mbocuan Project

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+$harga_tshirt = "Rp 150.000,00";
+$nama_tshirt_putih = "White T-Shirt";
+$nama_tshirt_merah = "Red T-Shirt";
+$nama_tshirt_abu = "Gray T-Shirt";
+$nama_tshirt_hitam = "Black T-Shirt"
+?>
